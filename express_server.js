2f808d064d65e80c4bb2d0aa9dae0ae5641dcb8f
hyuntony <ankim90@gmail.com>
2021-06-07 19:30:31 -0400

    added random string generator

--- express_server.js
+++ express_server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
+
+const generateRandomString = function() {
+  const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+  }
+  return result;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -13,6 +25,21 @@ app.get('/', (req, res) => {
   res.send("Hello");
 });
 
+app.get('/urls', (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
+
+});
+
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
@@ -21,6 +48,11 @@ app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send("OK");
+});
+
 app.listen(PORT, () => {
-  console.log('Example app listening on port ${PORT}');
+  console.log(`Example app listening on port ${PORT}`);
 });
